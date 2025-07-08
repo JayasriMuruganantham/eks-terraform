490bb190cc31ab6287fbe196d4b8375eddfa31a6
JayasriMuruganantham <91053425+JayasriMuruganantham@users.noreply.github.com>
2025-07-08 16:08:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
@@ -19,6 +30,26 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "demo-java-cluster"
+  cluster_version = "1.28"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    app_nodes = {
+      desired_capacity = 2
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
+}
+erraform-aws-modules/eks/aws"
   version = "20.8.4"
 
   cluster_name    = "demo-java-cluster"

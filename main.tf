838b6db3f2a8c72fb48811bde76829e73ac07d7e
JayasriMuruganantham <91053425+JayasriMuruganantham@users.noreply.github.com>
2025-07-08 15:57:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = ["us-east-1a", "us-east-1b"]
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "demo-java-cluster"
+  cluster_version = "1.28"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    app_nodes = {
+      desired_capacity = 2
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
+}
